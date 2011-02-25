3b7666464fea638964d3f2a2af4172d0f4c2002d
sambolmd@gmail.com <sambolmd@gmail.com@e8cf01fe-9b39-476f-ecb6-098cede329a4>
2011-02-25 18:34:54 +0000

    Completed Person2.java

--- M4/src/edu/gatech/oad/antlab/person/Person2.java
+++ M4/src/edu/gatech/oad/antlab/person/Person2.java
@@ -1,4 +1,5 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Random;
 
 /**
  *  A simple class for person 2
@@ -19,6 +20,7 @@ public class Person2 {
 	 public Person2(String pname) {
 	   name = pname;
 	 }
+	 
 	/**
 	 * This method should take the string
 	 * input and return its characters in
@@ -30,8 +32,32 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+	
+		// Random number generator
+		Random rand = new Random();
+		// Temporary value for new string
+		int temp;
+		// Input string as array
+		char[] ch = input.toCharArray();
+		// Array for random string
+		char[] random = new char[input.length()];
+
+		// Initialize new array with spaces
+		for (int i = 0; i < input.length(); i++)
+			random[i] = ' ';
+
+		// For each character in the input array, find a new, random position
+		for (int j = 0; j < input.length(); j++) {
+			temp = rand.nextInt(input.length());
+			while (random[temp] != ' ') {
+				temp = rand.nextInt(input.length());
+			}
+			random[temp] = ch[j];
+		}
+
+		// String to return
+		String message = new String(random);
+		return message;
 	}
 	/**
 	 * Return a string rep of this object
