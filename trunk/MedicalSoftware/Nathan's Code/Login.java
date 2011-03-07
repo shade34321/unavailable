e6ae0166a0052ac71d155994c327e6932791d003
aronijin@gmail.com <aronijin@gmail.com@e8cf01fe-9b39-476f-ecb6-098cede329a4>
2011-03-07 18:57:38 +0000

    Updating some code for transfer to another computer

--- trunk/MedicalSoftware/Nathan's Code/Login.java
+++ trunk/MedicalSoftware/Nathan's Code/Login.java
@@ -5,30 +5,38 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class Login {
 	private Info info;
-	private AVL<String, Info> information;
+	private SystemAdmin systemAdmin;
+	private Doctor doctor;
+	private Nurse nur;
+	private Patient pp;
+	private AVL<String, SystemAdmin> admin;
+	private AVL<String, Patient> patient;
+	private AVL<String, Nurse> nurse;
+	private AVL<String, Doctor> doc;
 	
 	Login() {
-		information = new AVL<String, Info>();
+		admin = new AVL<String, SystemAdmin>();
+		doc = new AVL<String, Doctor>();
+		patient = new AVL<String, Patient>();
+		nurse = new AVL<String, Nurse>();
 	}
-	
+
 	public void run() throws IOException {
 		setup();
 	}
-	
+
 	private void setup() throws IOException {
 		String name;
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nathanial\\Documents\\CS-2340\\Info.txt"));
-			while((name = br.readLine()) != null)
-			{
-				info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
-				information.insert(name, info);	
+			while ((name = br.readLine()) != null) {
+				this.info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
+				this.systemAdmin.setInfo(info);
+				this.admin.insert(name, systemAdmin);
 			}
-		}
-		catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			System.out.println("Error no such file found!");
 		}
 	}
