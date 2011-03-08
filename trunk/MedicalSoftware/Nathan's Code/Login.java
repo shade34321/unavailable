e5944c0a22e0c8215a8a6151adfbc36167551c12
aronijin@gmail.com <aronijin@gmail.com@e8cf01fe-9b39-476f-ecb6-098cede329a4>
2011-03-08 01:54:31 +0000

    Updating Login, MedicalMain, and SystemAdmin to the repository.  Not finished!

--- trunk/MedicalSoftware/Nathan's Code/Login.java
+++ trunk/MedicalSoftware/Nathan's Code/Login.java
@@ -1,43 +1,35 @@
 package MedicalSoftware;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Login {
-	private Info info;
 	private SystemAdmin systemAdmin;
-	private Doctor doctor;
+	private Doctor doc;
 	private Nurse nur;
-	private Patient pp;
+	private Patient person;
 	private AVL<String, SystemAdmin> admin;
 	private AVL<String, Patient> patient;
 	private AVL<String, Nurse> nurse;
-	private AVL<String, Doctor> doc;
+	private AVL<String, Doctor> doctor;
 	
 	Login() {
-		admin = new AVL<String, SystemAdmin>();
-		doc = new AVL<String, Doctor>();
-		patient = new AVL<String, Patient>();
-		nurse = new AVL<String, Nurse>();
+		this.admin = new AVL<String, SystemAdmin>();
+		this.doc = new AVL<String, Doctor>();
+		this.patient = new AVL<String, Patient>();
+		this.nurse = new AVL<String, Nurse>();
 	}
 
-	public void run() throws IOException {
-		setup();
+	Login(AVL<String, SystemAdmin> admin, AVL<String, Doctor> doctor, AVL<String, Nurse> nurse, AVL<String, Patient> patient) {
+		this.admin = admin;
+		this.doctor = doctor;
+		this.nurse = nurse;
+		this.patient = patient;
+	}
+	
+	public void run() {
+		loginUser();
 	}
 
-	private void setup() throws IOException {
-		String name;
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nathanial\\Documents\\CS-2340\\Info.txt"));
-			while ((name = br.readLine()) != null) {
-				this.info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
-				this.systemAdmin.setInfo(info);
-				this.admin.insert(name, systemAdmin);
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Error no such file found!");
-		}
+	private void loginUser() {
+		String userName;
+		String password;
 	}
 }
--- trunk/MedicalSoftware/Nathan's Code/MedicalMain.java
+++ trunk/MedicalSoftware/Nathan's Code/MedicalMain.java
@@ -1,10 +1,79 @@
 package MedicalSoftware;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class MedicalMain {
+	
+	private static Info info;
+	private static SystemAdmin systemAdmin;
+	private static Doctor doctor;
+	private static Nurse nur;
+	private static Patient person;
+	private static AVL<String, SystemAdmin> admin;
+	private static AVL<String, Patient> patient;
+	private static AVL<String, Nurse> nurse;
+	private static AVL<String, Doctor> doc;
+	
+	// Main method to run
 	public static void main(String[] args) throws IOException {
+		setup();
 		Login lg = new Login();
-		lg.run();
+		lg.run(admin, docotr, nurse, patient);
+	}
+	
+	// Loads the AVL trees with user information of all types
+	private static void setup() throws IOException {
+		String name;
+		
+		// Loads SystemAdmin information into tree
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nathanial\\Documents\\CS-2340\\Admin.txt"));
+			while ((name = br.readLine()) != null) {
+				info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
+				systemAdmin.setInfo(info);
+				admin.insert(name, systemAdmin);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error no such file found!");
+		}
+		
+		// Loads Docotr information into tree
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nathanial\\Documents\\CS-2340\\Doctor.txt"));
+			while ((name = br.readLine()) != null) {
+				info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
+				doctor.setInfo(info);
+				doc.insert(name, doctor);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error no such file found!");
+		}
+		
+		// Loads Nurse information into tree
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nathanial\\Documents\\CS-2340\\Nurse.txt"));
+			while ((name = br.readLine()) != null) {
+				info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
+				nur.setInfo(info);
+				nurse.insert(name, nur);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error no such file found!");
+		}
+		
+		// Loads Patient information into tree
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Nathanial\\Documents\\CS-2340\\Patient.txt"));
+			while ((name = br.readLine()) != null) {
+				info = new Info(name, br.readLine(), br.readLine(), br.readLine(), br.readLine(), br.readLine(), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), Integer.parseInt(br.readLine()), false);
+				person.setInfo(info);
+				patient.insert(name, patient);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error no such file found!");
+		}
 	}
 }
