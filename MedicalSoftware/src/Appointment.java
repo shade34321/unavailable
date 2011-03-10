8d9f6747cdfcf087f1eadb301b1b9c4c1d773865
shade34321 <shade34321@e8cf01fe-9b39-476f-ecb6-098cede329a4>
2011-03-10 03:49:31 +0000

    Updated Appointment

--- MedicalSoftware/src/Appointment.java
+++ MedicalSoftware/src/Appointment.java
@@ -8,23 +8,23 @@ public class Appointment {
 	private ArrayList<Appt> App;
 	private Comparator compare;
 	
-	appointment(){
+	Appointment(){
 		App = new ArrayList<Appt>();
 	}
 	public void create(int date, int time, String name, String doctor, String reason){
 		Appt a = new Appt(date, time, name, doctor, reason);
-		app.add(a);
+		App.add(a);
 		
 	}
 	
 	public void cancel(int date, int time, String name, String doctor, String reason){
-		int size = app.size();
+		int size = App.size();
 		for(int i = 0;i<size;i++){
-			if(app(i).getName() == name){
-				if(app(i).getDate() == date){
-					if(app(i).getTime() == time){
-						if(app(i).getDoctor() == doctor){
-							app.remove(i);
+			if(App.get(i).getName() == name){
+				if(App.get(i).getDate() == date){
+					if(App.get(i).getTime() == time){
+						if(App.get(i).getDoctor() == doctor){
+							App.remove(i);
 						}
 					}
 				}
