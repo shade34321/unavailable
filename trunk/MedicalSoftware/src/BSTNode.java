5279da263263b03912efb3399643c831f17375a3
sambolmd@gmail.com <sambolmd@gmail.com@e8cf01fe-9b39-476f-ecb6-098cede329a4>
2011-04-07 23:23:50 +0000

    Code Pro changes, added .jar files for GUIs

---
+++ trunk/MedicalSoftware/src/BSTNode.java
@@ -0,0 +1,73 @@
+package MedicalSoftware;
+
+public class BSTNode<K extends Comparable<K>, V> {
+	private K key;
+	
+	private V value;
+	
+	private BSTNode<K, V> left;
+	
+	private BSTNode<K, V> right;
+	
+	private int bf;
+	
+	private int height;
+
+	public BSTNode(K key, V value) {
+		this.key = key;
+		this.value = value;
+		this.height = 0;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	public BSTNode<K, V> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BSTNode<K, V> left) {
+		this.left = left;
+	}
+
+	public BSTNode<K, V> getRight() {
+		return right;
+	}
+
+	public void setRight(BSTNode<K, V> right) {
+		this.right = right;
+	}
+
+	public String toString() {
+		return key.toString() + " ";
+	}
+
+	public int getBF() {
+		return bf;
+	}
+
+	public void setBF(int bf) {
+		this.bf = bf;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+}
